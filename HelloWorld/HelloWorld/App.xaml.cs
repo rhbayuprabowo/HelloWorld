67ac2ef4bac87caeeeba5be32036ea6104f314ee
R. H. Bayu Prabowo <rh.bayu.prabowo@gmail.com>
2018-12-30 22:41:55 +0700

    tambah file exercise grid #1

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new StackLayoutExercise2();
+            MainPage = new GridPageExercise1();
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/GridPageExercise1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GridPageExercise1 : ContentPage
+	{
+		public GridPageExercise1()
+		{
+			InitializeComponent ();
+		}
+	}
+}
